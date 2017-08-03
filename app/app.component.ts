442f95d71ff70734f45d6acbd0f06281ae1c6d5f
askgoliath <dvdaboh@yahoo.com>
2017-08-02 21:19:59 -0400

    test viewing changes on mobile device

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,6 @@ import {Component} from 'angular2/core';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>Starting Line Changes</h1>'
+    template: '<h1>One step for dave. A leap for mandem</h1>'
 })
 export class AppComponent { }
