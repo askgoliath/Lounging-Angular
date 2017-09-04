159895229bc815c55e79c89c978813178d6dda16
askgoliath <dvdaboh@yahoo.com>
2017-09-04 12:18:21 -0400

    sanity check

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,7 +3,7 @@ import {Component} from 'angular2/core';
 @Component({
     selector: 'my-app',
     template: '<h1>One step for dave. A leap for mandem.</h1> \
-        <p> Test cache busting again and again </p> \
+        <p> Test cache busting again and again. sanity check </p> \
     '
 })
 export class AppComponent { }
