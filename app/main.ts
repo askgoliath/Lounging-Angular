09f870ca4b7a0b913b3b1dcf56f4ef24e5cf47fa
askgoliath <dvdaboh@yahoo.com>
2018-03-12 21:29:36 -0400

    attempt to fix compatibility to angular 5

--- app/main.ts
+++ app/main.ts
@@ -1,4 +1,22 @@
-import {bootstrap}    from 'angular2/platform/browser';
-import {AppComponent} from './app.component';
+/* JavaScript imports */
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
-bootstrap(AppComponent);
+import { AppComponent } from './app.component';
+
+/* the AppModule class with the @NgModule decorator */
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
